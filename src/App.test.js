68302734380600668f6aa928ae70059455d9b6e7
Sine <sinje@aarhus.dk>
2021-07-12 12:51:14 +0200

    AR-584: clean up and stylelint

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,1 @@
-describe('Test shell', () => {
-
-});
+describe("Test", () => {});
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import { React } from "react";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
-
+      App!
     </div>
   );
 }
