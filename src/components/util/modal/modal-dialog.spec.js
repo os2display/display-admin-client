2285fcfefd9cacdec973b14c2e078d3fb91a61ea
Sine <sinje@aarhus.dk>
2021-07-21 14:06:32 +0200

    remove test, as it just repeats another test

--- src/components/util/modal/modal-dialog.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Modal loads", () => {
-  it("It loads", () => {
-    cy.visit("/tags");
-    cy.get('[type="checkbox"]').check();
-    cy.get("#delete-button").click();
-    cy.get(".modal-container").should("have.css", "position", "absolute");
-  });
-});
