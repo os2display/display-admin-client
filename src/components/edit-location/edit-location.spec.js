05840276ec28ecc733db5bbc0cfa77ae263f0ae9
Sine <sinje@aarhus.dk>
2021-07-22 21:30:33 +0200

    AR-598: fixed tests

--- src/components/edit-location/edit-location.spec.js
+++ src/components/edit-location/edit-location.spec.js
@@ -1,39 +1,35 @@
-describe("Edit screen page loads", () => {
+describe("Edit location page loads", () => {
   it("It loads", () => {
-    cy.visit("/screen/new");
+    cy.visit("/location/new");
     cy.get("h1").should("not.be.empty");
     cy.get("h1")
       .invoke("text")
-      .should("match", /^Opret ny skærm/);
-    cy.visit("/screen/76");
+      .should("match", /^Opret ny lokation/);
+    cy.visit("/location/76");
     cy.get("h1").should("not.be.empty");
     cy.get("h1")
       .invoke("text")
-      .should("match", /^Rediger denne skærm: Asoka/);
+      .should("match", /^Rediger lokationen: Dokk1/);
   });
 
-  it("It loads drag and drop table", () => {
-    cy.visit("/screen/32");
-    cy.get("tbody").find("tr td").should("have.length", 3);
-  });
 
   it("It removes from list", () => {
-    cy.visit("/screen/32");
-    cy.get("tbody").find("tr td").should("have.length", 3);
+    cy.visit("/location/32");
+    cy.get("tbody").find("tr td").should("have.length", 5);
     cy.get("tbody").find("tr td button").eq(1).click();
     cy.get("tbody").should("not.exist");
 
   });
-
   it("It goes back", () => {
-    cy.visit("/");
-    cy.visit("/screen/new");
+    cy.visit("/locations");
+    cy.visit("/location/new");
     cy.get(".container")
       .find("button")
       .eq(0)
       .invoke("text")
       .should("match", /^Annuller/);
-    cy.get(".container").find("button").eq(0).click();
+    cy.get("input").type("Hello, World");
+    cy.get("#save_location").click();
     cy.get(".container")
       .find("button")
       .eq(1)
