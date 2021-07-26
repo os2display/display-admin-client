ef375ed59d92e69f4824399733532e1ef67ab928
Sine <sinje@aarhus.dk>
2021-07-26 15:18:23 +0200

    AR-578: fixed an error in test

--- src/components/edit-screen/edit-screen.spec.js
+++ src/components/edit-screen/edit-screen.spec.js
@@ -17,22 +17,6 @@ describe("Edit screen page loads", () => {
     cy.get("tbody").find("tr td").should("have.length", 4);
   });
 
-  it("It loads delete modal", () => {
-    cy.visit("/screen/32");
-    cy.get("tbody").find("tr td button").eq(1).click();
-    cy.get(".modal-container").should("have.css", "position", "absolute");
-    cy.get(".modal-container").find("button").should("have.length", 2);
-    cy.get(".modal-container")
-      .find("button")
-      .first()
-      .invoke("text")
-      .should("match", /^Nej/);
-    cy.get(".modal-container")
-      .find("button")
-      .last()
-      .invoke("text")
-      .should("match", /^Ja/);
-  });
 
   it("It goes back", () => {
     cy.visit("/");
