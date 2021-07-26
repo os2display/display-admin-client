de18a74246037cb88488547d6bee2273e4a571fb
Sine <sinje@aarhus.dk>
2021-07-26 09:58:37 +0200

    AR-575: added a componenter to render form elements

---
+++ src/components/util/forms/render-form-element.jsx
@@ -0,0 +1,116 @@
+import { React } from "react";
+import PropTypes from "prop-types";
+import ImageUploader from "../image-uploader/image-uploader";
+import FormCheckbox from "./form-checkbox";
+import FormInput from "./form-input";
+import Select from "./select";
+
+/**
+ * @param {object} props
+ * The props.
+ * @param {Array} props.data
+ * The data to render in the form element.
+ * @param {Function} props.requiredFieldCallback
+ * If the form is required, a callback to add to validation.
+ * @param {Array} props.errors
+ * An error list, if there are validation errors.
+ * @param {Function} props.onChange
+ * Callback, if the value of the field changes.
+ * @param {object} props.formStateObject
+ * The form state.
+ * @returns {object}
+ * A form element.
+ */
+function RenderFormElement({
+  data,
+  requiredFieldCallback,
+  errors,
+  onChange,
+  formStateObject,
+}) {
+  if (data.required) {
+    requiredFieldCallback(data.name);
+  }
+  /**
+   * @param {object} formData
+   * The data for form input.
+   * @returns {object|string}
+   * returns a rendered jsx object.
+   */
+  function renderElement(formData) {
+    switch (formData.input) {
+      case "input":
+        return (
+          <FormInput
+            name={formData.name}
+            type={formData.type}
+            errors={formData.required ? errors : null}
+            label={formData.label}
+            placeholder={formData.placeholder}
+            value={formStateObject[formData.name]}
+            onChange={onChange}
+          />
+        );
+      case "checkbox":
+        return (
+          <FormCheckbox
+            label={formData.label}
+            onChange={onChange}
+            name={formData.name}
+            helpText={formData.helpText}
+            value={formStateObject[formData.name]}
+          />
+        );
+      case "header":
+        return <h2>{formData.text}</h2>;
+      case "select":
+        return (
+          <Select
+            helpText={formData.helpText}
+            value={formStateObject[formData.name]}
+            name={formData.name}
+            options={formData.options}
+            onChange={onChange}
+            label={formData.label}
+            errors={formData.required ? errors : null}
+          />
+        );
+      case "image":
+        if (formStateObject[formData.name]) {
+          return (
+            <ImageUploader
+              handleImageUpload={onChange}
+              image={formStateObject[formData.name]}
+            />
+          );
+        }
+        break;
+      default:
+        return <></>;
+    }
+  }
+  return <>{renderElement(data)}</>;
+}
+
+RenderFormElement.defaultProps = {
+  errors: [],
+};
+
+RenderFormElement.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({ input: PropTypes.string.isRequired })
+  ).isRequired,
+  errors: PropTypes.arrayOf(PropTypes.string),
+  formStateObject: PropTypes.shape({
+    input: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+  }).isRequired,
+  requiredFieldCallback: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default RenderFormElement;
