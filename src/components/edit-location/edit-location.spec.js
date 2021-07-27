d15cf19c13a2fd24a1b4f66b6c26c4954d85cb4b
Sine <sinje@aarhus.dk>
2021-07-27 12:59:34 +0200

    AR-598: fixed test

--- src/components/edit-location/edit-location.spec.js
+++ src/components/edit-location/edit-location.spec.js
@@ -15,7 +15,7 @@ describe("Edit location page loads", () => {
   it("It removes from list", () => {
     cy.visit("/location/32");
     cy.get("tbody").find("tr td").should("have.length", 5);
-    cy.get("tbody").find("tr td button").eq(1).click();
+    cy.get("tbody").find("tr td button").eq(0).click();
     cy.get("tbody").should("not.exist");
   });
 });
