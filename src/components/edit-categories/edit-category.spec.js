56dfb67ffb88a1f0c5152f39ea99e0a6618b9fdf
Sine <sinje@aarhus.dk>
2021-07-27 10:05:08 +0200

    AR-575: edit category spec now passes

--- src/components/edit-categories/edit-category.spec.js
+++ src/components/edit-categories/edit-category.spec.js
@@ -1,5 +1,5 @@
-describe("Edit category page loads", () => {
-  it("It loads", () => {
+describe("Edit category page tests", () => {
+  it("It loads a category", () => {
     cy.visit("/category/new");
     cy.get("h1").should("not.be.empty");
     cy.get("h1")
@@ -12,39 +12,28 @@ describe("Edit category page loads", () => {
       .should("match", /^Rediger kategorien: Lotstring/);
   });
 
-
   it("It validates new category", () => {
     cy.visit("/category/new");
-    cy.get(".container")
-      .find("button")
-      .eq(1)
+    cy.get("#save_category")
       .invoke("text")
       .should("match", /^Gem kategorien/);
-    cy.get(".container").find("button").eq(1).click();
-    cy.get(".container")
-      .find("button")
-      .eq(1)
+    cy.get("#save_category").click();
+    cy.get("#save_category")
       .invoke("text")
       .should("match", /^Gem kategorien/);
     cy.get("input").type("Hello, World");
-    cy.get(".container").find("button").eq(1).click();
-    cy.get(".container")
-      .find("button")
-      .eq(1)
-      .invoke("text")
-      .should("match", /^Konsolider/);
+    cy.get("#save_category").click();
+    cy.get("#save_category").should("not.exist");
   });
 
   it("It validates already existing category", () => {
     cy.visit("/category/32");
     cy.get("#category_name").clear();
-    cy.get(".container")
-      .find("#save_category")
+    cy.get("#save_category")
       .invoke("text")
       .should("match", /^Gem kategori/);
     cy.get("#save_category").click();
-    cy.get(".container")
-      .find("#save_category")
+    cy.get("#save_category")
       .invoke("text")
       .should("match", /^Gem kategori/);
     cy.get("#category_name").type("Hello, World");
