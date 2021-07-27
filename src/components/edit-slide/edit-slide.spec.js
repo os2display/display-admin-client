a863df2d3f4b133f02b8a1bc97c8b752be2f3214
Sine <sinje@aarhus.dk>
2021-07-27 09:19:54 +0200

    AR-575: remove it goes back tests

--- src/components/edit-slide/edit-slide.spec.js
+++ src/components/edit-slide/edit-slide.spec.js
@@ -11,20 +11,4 @@ describe("Edit slide page loads", () => {
       .invoke("text")
       .should("match", /^Rediger følgende slide: Roderigo/);
   });
-  it("It goes back", () => {
-    cy.visit("/slides");
-    cy.visit("/slide/new");
-    cy.get(".container")
-      .find("button")
-      .eq(0)
-      .invoke("text")
-      .should("match", /^Annuller/);
-    cy.get("input").type("Hello, World");
-    cy.get("#slide_template").select("Text-and-image");
-    cy.get("#title").type("Hello, World");
-    cy.get("#box-align").select("Toppen");
-    cy.get("#duration").type(123);
-    cy.get("#save_slide").click();
-    cy.get("#save_slide").should("not.exist");
-  });
 });
