a313cc71bd1cfd07b23db66022673fd040b55bca
Sine <sinje@aarhus.dk>
2021-07-27 10:02:42 +0200

    AR-575: tag spec now passes

--- src/components/edit-tag/edit-tag.spec.js
+++ src/components/edit-tag/edit-tag.spec.js
@@ -1,45 +1,5 @@
-describe("Edit tag page loads", () => {
-
-  it("It validates new tag", () => {
-    cy.visit("/tag/new");
-    cy.get(".container")
-      .find("button")
-      .eq(1)
-      .invoke("text")
-      .should("match", /^Gem tag/);
-    cy.get(".container").find("button").eq(1).click();
-    cy.get(".container")
-      .find("button")
-      .eq(1)
-      .invoke("text")
-      .should("match", /^Gem tag/);
-    cy.get("input").type("Hello, World");
-    cy.get(".container").find("button").eq(1).click();
-    cy.get(".container")
-      .find("button")
-      .eq(1)
-      .invoke("text")
-      .should("match", /^Konsolider/);
-  });
-
-  it("It validates already existing tag", () => {
-    cy.visit("/tag/32");
-    cy.get("#tag_name").clear();
-    cy.get(".container")
-      .find("#save_tag")
-      .invoke("text")
-      .should("match", /^Gem tag/);
-    cy.get("#save_tag").click();
-    cy.get(".container")
-      .find("#save_tag")
-      .invoke("text")
-      .should("match", /^Gem tag/);
-    cy.get("#tag_name").type("Hello, World");
-    cy.get("#save_tag").click();
-    cy.get("#save_tag").should("not.exist");
-  });
-
-  it("It loads", () => {
+describe("Edit tag page tests", () => {
+  it("It loads a tags", () => {
     cy.visit("/tag/new");
     cy.get("h1").should("not.be.empty");
     cy.get("h1")
@@ -51,4 +11,34 @@ describe("Edit tag page loads", () => {
       .invoke("text")
       .should("match", /^Rediger følgende tag: matrices/);
   });
+
+  it("It validates new tag", () => {
+    cy.visit("/tag/new");
+    cy.get("#save_tag")
+      .invoke("text")
+      .should("match", /^Gem tag/);
+    cy.get("#save_tag").click();
+    cy.get("#save_tag")
+      .invoke("text")
+      .should("match", /^Gem tag/);
+    cy.get("input").type("Hello, World");
+    cy.get("#save_tag").click();
+    cy.get("#save_tag").should("not.exist")
+  });
+
+  it("It validates already existing tag", () => {
+    cy.visit("/tag/32");
+    cy.get("#tag_name").clear();
+    cy.get("#save_tag")
+      .invoke("text")
+      .should("match", /^Gem tag/);
+    cy.get("#save_tag").click();
+    cy.get("#save_tag")
+      .invoke("text")
+      .should("match", /^Gem tag/);
+    cy.get("#tag_name").type("Hello, World");
+    cy.get("#save_tag").click();
+    cy.get("#save_tag").should("not.exist");
+  });
+
 });
