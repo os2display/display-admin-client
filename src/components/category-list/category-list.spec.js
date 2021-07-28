c6bc59718ae791775d312e733230162f9518c65b
Sine <sinje@aarhus.dk>
2021-07-28 08:42:12 +0200

    AR-616L added category list tests

--- src/components/category-list/category-list.spec.js
+++ src/components/category-list/category-list.spec.js
@@ -4,7 +4,25 @@ describe("Tags list loads", () => {
     cy.get("table").find("tbody").should("not.be.empty");
     cy.get("tbody").find("tr td").should("have.length", 60);
   });
+  it("It opens info modal", () => {
+    cy.visit("/categories");
+    cy.get("#info-modal").should("not.exist")
+    cy.get("tbody").find("tr td button").eq(0).click();
+    cy.get("#info-modal").should("exist")
+  });
 
+  it("It goes to edit", () => {
+    cy.visit("/categories");
+    cy.get("#categoryName").should("not.exist")
+    cy.get("tbody").find("tr td a").eq(0).click();
+    cy.get("#categoryName").should("exist")
+  });
+  it("It opens delete modal", () => {
+    cy.visit("/categories");
+    cy.get("#delete-modal").should("not.exist")
+    cy.get("tbody").find("tr td button").eq(1).click();
+    cy.get("#delete-modal").should("exist")
+  });
   it("The column headers are correct", () => {
     cy.visit("/categories");
     cy.get("thead").find("th").eq(0).invoke("text").should("match", /^Valg/);
