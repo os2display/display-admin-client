ec96d9f16e995028f45aab98f0f8d0ad702d688f
Sine <sinje@aarhus.dk>
2021-07-29 14:07:42 +0200

    AR-616: fixed info modal test

--- src/components/info-modal/info-modal.spec.js
+++ src/components/info-modal/info-modal.spec.js
@@ -7,7 +7,7 @@ describe("Info modal loads", () => {
       .invoke("text")
       .should(
         "match",
-        /^ObsKategorien er på de følgende spillelister: dynamic, Mandatory, Enhanced, explicit and policyLuk dialog/
+        /^ObsKategorien er på de følgende spillelister: dynamic, Mandatory, Enhanced, explicit og policyLuk dialog/
       );
   });
 });
