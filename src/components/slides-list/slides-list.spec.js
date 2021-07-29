1e4c822b1b76250dd61f00be36e5a8ff4405cc13
Sine <sinje@aarhus.dk>
2021-07-29 14:08:54 +0200

    AR-616: fixed slides list spec

--- src/components/slides-list/slides-list.spec.js
+++ src/components/slides-list/slides-list.spec.js
@@ -14,7 +14,7 @@ describe("Slides list tests", () => {
   it("It opens delete modal (slides list)", () => {
     cy.visit("/slides");
     cy.get("#delete-modal").should("not.exist");
-    cy.get("tbody").find("tr td button").eq(0).click();
+    cy.get("tbody").find("tr td button").eq(1).click();
     cy.get("#delete-modal").should("exist");
   });
 
