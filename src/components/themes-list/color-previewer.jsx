f4fa91e2256f6a170a2ba49a1b2054ead9c46e0d
Sine <sinje@aarhus.dk>
2021-08-03 12:09:54 +0200

    AR-601: added color previewer

---
+++ src/components/themes-list/color-previewer.jsx
@@ -0,0 +1,51 @@
+import { React } from "react";
+import PropTypes from "prop-types";
+import "./color-previewer.scss";
+
+/**
+ * A link for the list
+ *
+ * @param {object} props
+ * Props.
+ * @param {object} props.data
+ * Data data object with colors.ejs
+ * @returns {object}
+ * A link for the list.
+ */
+function ColorPreviewer({ data }) {
+  return (
+    <div className="d-flex flex-column">
+      {data.colors.primary && (
+        <div
+          className="preview"
+          style={{ backgroundColor: `${data.colors.primary}` }}
+        >
+          {data.colors.primary}
+        </div>
+      )}
+      {data.colors.secondary && (
+        <div
+          className="preview"
+          style={{ backgroundColor: `${data.colors.secondary}` }}
+        >
+          {data.colors.secondary}
+        </div>
+      )}
+      {data.colors.tertiary && (
+        <div
+          className="preview"
+          style={{ backgroundColor: `${data.colors.tertiary}` }}
+        >
+          {data.colors.tertiary}
+        </div>
+      )}
+    </div>
+  );
+}
+
+ColorPreviewer.propTypes = {
+  data: PropTypes.shape({ name: PropTypes.string, id: PropTypes.number })
+    .isRequired,
+};
+
+export default ColorPreviewer;
