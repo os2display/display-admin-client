d424d1ef7fba868d02ca89100354e58b4fd38079
Sine <sinje@aarhus.dk>
2021-08-03 08:31:38 +0200

    AR-576: switched to tests whichs seem to affect them

--- src/components/edit-slide/edit-slide.spec.js
+++ src/components/edit-slide/edit-slide.spec.js
@@ -1,15 +1,16 @@
 describe("Edit slide page tests", () => {
+
   it("It loads", () => {
-    cy.visit("/slide/new");
-    cy.get("h1").should("not.be.empty");
-    cy.get("h1")
-      .invoke("text")
-      .should("match", /^Opret nyt slide/);
     cy.visit("/slide/76");
     cy.get("h1").should("not.be.empty");
     cy.get("h1")
       .invoke("text")
       .should("match", /^Rediger følgende slide: Roderigo/);
+    cy.visit("/slide/new");
+    cy.get("h1").should("not.be.empty");
+    cy.get("h1")
+      .invoke("text")
+      .should("match", /^Opret nyt slide/);
   });
 
   it("It validates new slide", () => {
