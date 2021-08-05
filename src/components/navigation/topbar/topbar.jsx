742521e59498bafe726d0db89d66ee2cba585c65
Jesper Pedersen <pejes@aarhus.dk>
2021-08-05 12:28:49 +0200

    AR-619: Topbar styling. Add responsiveness

--- src/components/navigation/topbar/topbar.jsx
+++ src/components/navigation/topbar/topbar.jsx
@@ -7,6 +7,7 @@ import Navbar from "react-bootstrap/navbar";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavItems from "../nav-items/nav-items";
 import {
   faPlusCircle,
   faPhotoVideo,
@@ -17,6 +18,7 @@ import {
   faSignOutAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import SearchBox from "../../util/search-box/search-box";
+import "./topbar.scss";
 /**
  * The side bar component.
  *
@@ -26,91 +28,72 @@ import SearchBox from "../../util/search-box/search-box";
 function TopBar() {
   const [t] = useTranslation("common");
   return (
-    <Navbar bg="dark" expand="lg" className="border-bottom">
-      <Col sm={2}>
-        <Navbar.Brand href="/" className="text-white">
-          {t("topbar.brand")}
-        </Navbar.Brand>
-      </Col>
-      <Col>
-        <Row>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Col sm="auto" className="">
-              {/* TODO: Global searchbox: results show as list in popover when typing more than 3 characters */}
-              <SearchBox />
-            </Col>
-            <Col sm="auto" className="ms-auto">
-              <Nav>
-                <Dropdown className="me-md-3">
-                  <Dropdown.Toggle variant="primary" id="topbar_add">
-                    <FontAwesomeIcon className="me-1" icon={faPlusCircle} />
-                    {t("topbar.add")}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <Link
-                        id="nav-items_slides"
-                        className="nav-link"
-                        to="/slides"
-                      >
-                        <FontAwesomeIcon className="me-2" icon={faPhotoVideo} />
-                        {t("topbar.add_slide")}
-                      </Link>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <Link
-                        id="nav-items_screens_screens"
-                        className="nav-link"
-                        to="/screens"
-                      >
-                        <FontAwesomeIcon className="me-2" icon={faDesktop} />
-                        {t("topbar.add_screen")}
-                      </Link>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <Link
-                        id="nav-items_playlists_playlists"
-                        className="nav-link"
-                        to="/playlists"
-                      >
-                        <FontAwesomeIcon className="me-2" icon={faStream} />
-                        {t("topbar.add_playlist")}
-                      </Link>
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                <Nav.Item className="me-md-3">
-                  <Link id="topbar-faq" className="btn btn-primary" to="/faq">
-                    <FontAwesomeIcon
-                      className="me-2 "
-                      icon={faQuestionCircle}
-                    />
-                    <span className="visually-hidden">{t("topbar.faq")}</span>
-                  </Link>
-                </Nav.Item>
-                <Dropdown>
-                  <Dropdown.Toggle variant="outline-light" id="topbar_user">
-                    <FontAwesomeIcon
-                      className="me-1 fa-lg"
-                      icon={faUserCircle}
-                    />
-                    {t("topbar.user")}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <Link id="topbar_signout" to="/signout">
-                        <FontAwesomeIcon className="me-1" icon={faSignOutAlt} />
-                        {t("topbar.signout")}
-                      </Link>
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Nav>
-            </Col>
-          </Navbar.Collapse>
-        </Row>
-      </Col>
+    <Navbar variant="dark" bg="dark" expand="lg">
+      <Navbar.Brand href="/" className="text-white col-lg-2">
+        {t("topbar.brand")}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {/* TODO: Global searchbox: results show as list in popover when typing more than 3 characters */}
+        <SearchBox />
+        <Nav variant="dark" className="topbar-nav d-md-none">
+          <NavItems />
+        </Nav>
+        <Nav className="ms-md-auto mt-3 mt-md-0">
+          <Dropdown className="me-md-3 mb-2 mb-md-0">
+            <Dropdown.Toggle variant="primary" id="topbar_add">
+              <FontAwesomeIcon className="me-1" icon={faPlusCircle} />
+              {t("topbar.add")}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Link
+                id="nav-items_slides"
+                className="dropdown-item"
+                to="/slides"
+              >
+                <FontAwesomeIcon className="me-2" icon={faPhotoVideo} />
+                {t("topbar.add_slide")}
+              </Link>
+
+              <Link
+                id="nav-items_screens_screens"
+                className="dropdown-item"
+                to="/screens"
+              >
+                <FontAwesomeIcon className="me-2" icon={faDesktop} />
+                {t("topbar.add_screen")}
+              </Link>
+
+              <Link
+                id="nav-items_playlists_playlists"
+                className="dropdown-item"
+                to="/playlists"
+              >
+                <FontAwesomeIcon className="me-2" icon={faStream} />
+                {t("topbar.add_playlist")}
+              </Link>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Nav.Item className="me-md-3 mb-2 mb-md-0">
+            <Link id="topbar-faq" className="btn btn-primary" to="/faq">
+              <FontAwesomeIcon icon={faQuestionCircle} />
+              <span className="visually-hidden">{t("topbar.faq")}</span>
+            </Link>
+          </Nav.Item>
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-light" id="topbar_user">
+              <FontAwesomeIcon className="me-1 fa-lg" icon={faUserCircle} />
+              {t("topbar.user")}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Link id="topbar_signout" className="dropdown-item" to="/signout">
+                <FontAwesomeIcon className="me-1" icon={faSignOutAlt} />
+                {t("topbar.signout")}
+              </Link>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
