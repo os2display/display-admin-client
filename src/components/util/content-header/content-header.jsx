cafa6d11cd9842be4421c96d3d44b5857e92e6c4
Jesper Pedersen <pejes@aarhus.dk>
2021-08-09 16:15:31 +0200

    AR-619: Fix imports

--- src/components/util/content-header/content-header.jsx
+++ src/components/util/content-header/content-header.jsx
@@ -1,8 +1,8 @@
 import { React } from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/button";
-import Col from "react-bootstrap/col";
-import Row from "react-bootstrap/row";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 /**
  * @param {object} props
