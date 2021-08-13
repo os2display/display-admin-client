da7999744a74eb96d871ae0e0112f3f197e7cadc
Jesper Pedersen <pejes@aarhus.dk>
2021-08-13 10:41:52 +0200

    AR-619: Test at #root instead of .App

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,6 @@
 describe("Simple app loads", () => {
   it("Loads and simple test", () => {
-    cy.visit("/tags");
-    cy.get(".App").should("not.be.empty");
+    cy.visit("/slides");
+    cy.get("#root").should("not.be.empty");
   });
 });
