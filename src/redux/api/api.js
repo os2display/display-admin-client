cd5f10c2e3eb3b85e47efb2869ff88acbe37475c
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-09-30 12:58:42 +0200

    AR-670: Added api layer

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,8 +1,8 @@
 import { api as generatedApi } from './api.generated'
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+/**
+ * Entry point for modifying api endpoint.
+ */
 export const api = generatedApi.enhanceEndpoints({
-  baseQuery: fetchBaseQuery({
-    baseUrl: '/fisk/',
-  }),
 })
