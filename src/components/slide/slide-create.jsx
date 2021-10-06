4f39cd18e02d49353107f5f85897f649195080fa
Sine <sinje@aarhus.dk>
2021-10-06 12:56:44 +0200

    AR-618: remove uncommented code

--- src/components/slide/slide-create.jsx
+++ src/components/slide/slide-create.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import idFromUrl from "../util/helpers/id-from-url";
 import {
   usePostV1SlidesMutation,
-  // usePutV1PlaylistsByIdSlideAndSlideIdMutation,
+  usePutV1PlaylistsByIdSlideAndSlideIdMutation,
 } from "../../redux/api/api.generated";
 import SlideForm from "./slide-form";
 
