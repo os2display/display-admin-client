91c9f0c72907ebc7d5603c723379a5a8e92cb4f9
Jesper Kristensen <cableman@linuxdev.dk>
2021-10-07 13:55:24 +0200

    AR-679: Added basic container setup

---
+++ infrastructure/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+APP_VERSION=develop
+VERSION=alpha
+
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-api-admin:${VERSION} --file="Dockerfile" .
+
+#docker push itkdev/os2display-api-admin:${VERSION}
