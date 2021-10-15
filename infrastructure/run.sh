f659dab644f7da24550c4ec9e2409dc9ac48a731
Jesper Kristensen <cableman@linuxdev.dk>
2021-10-15 12:28:49 +0200

    AR-702: Change docker image name

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -3,6 +3,6 @@
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-api-admin:${VERSION} --file="Dockerfile" .
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-admin-client:${VERSION} --file="Dockerfile" .
 
-docker push itkdev/os2display-api-admin:${VERSION}
+docker push itkdev/os2display-admin-client:${VERSION}
