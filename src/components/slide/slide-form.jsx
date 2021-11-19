4fbd086588ae5db5fb45f15a3bc3f788a512dc61
Sine <sinje@aarhus.dk>
2021-11-19 12:24:06 +0100

    AR-735: reset slide form

--- src/components/slide/slide-form.jsx
+++ src/components/slide/slide-form.jsx
@@ -1,10 +1,10 @@
 import { React, useEffect, useState } from "react";
-import { Button, Row, Col } from "react-bootstrap";
+import { Button, Spinner, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 import Form from "react-bootstrap/Form";
-import LoadingComponent from "../util/loading-component/loading-component";
+import Toast from "../util/toast/toast";
 import ContentBody from "../util/content-body/content-body";
 import MultiSelectComponent from "../util/forms/multiselect-dropdown/multi-dropdown";
 import ContentFooter from "../util/content-footer/content-footer";
@@ -23,7 +23,11 @@ import RemoteComponentWrapper from "./preview/remote-component-wrapper";
  * @param {object} props.slide The slide object to modify in the form.
  * @param {Function} props.handleInput Handles form input.
  * @param {Function} props.handleSubmit Handles form submit.
+ * @param {boolean} props.isSaving Is the form saving?
  * @param {string} props.headerText Headline text.
+ * @param {boolean | null} props.isSaveSuccess Is the save a success?
+ * @param {boolean | null} props.isLoading The data is loading.
+ * @param {Array} props.errors Array of errors.
  * @param {Function} props.handleContent Function for handling changes to content field
  * @param {Function} props.handleMedia Handle media field
  * @param {Array} props.mediaData Object of loaded media.
@@ -31,8 +35,6 @@ import RemoteComponentWrapper from "./preview/remote-component-wrapper";
  * @param {object} props.selectedTemplate Selected template.
  * @param {Function} props.selectTheme Function to handle select of theme.
  * @param {object} props.selectedTheme Selected theme.
- * @param {boolean} props.isLoading Indicator of whether the form is loading
- * @param {string} props.loadingMessage The loading message for the spinner
  * @returns {object} The slide form.
  */
 function SlideForm({
@@ -43,13 +45,14 @@ function SlideForm({
   handleSubmit,
   selectTemplate,
   selectedTemplate,
+  isSaving,
   headerText,
+  isSaveSuccess,
+  isLoading,
   mediaData,
-  mediaFields,
+  errors,
   selectTheme,
   selectedTheme,
-  isLoading,
-  loadingMessage,
 }) {
   const { t } = useTranslation("common");
   const history = useHistory();
@@ -129,131 +132,197 @@ function SlideForm({
   }
 
   return (
-    <>
-      <LoadingComponent isLoading={isLoading} loadingMessage={loadingMessage} />
-      <Form>
-        <h1>{headerText}</h1>
-        <ContentBody>
-          <FormInput
-            name="title"
-            type="text"
-            label={t("slide-form.slide-name-label")}
-            helpText={t("slide-form.slide-name-placeholder")}
-            value={slide.title || ""}
-            onChange={handleInput}
+    <Form>
+      <h1>{headerText}</h1>
+      {isLoading && (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            className="m-1"
           />
-        </ContentBody>
-        {templateOptions && (
+          {t("slide-form.loading")}
+        </>
+      )}
+      {isSaving && (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            className="m-1"
+          />
+          {t("slide-form.saving")}
+        </>
+      )}
+      {loadingTemplates && !isLoading && (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            className="m-1"
+          />
+          {t("slide-form.loading-templates")}
+        </>
+      )}
+      {loadingThemes && !isLoading && (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            className="m-1"
+          />
+          {t("slide-form.loading-themes")}
+        </>
+      )}
+      {!isLoading && (
+        <>
           <ContentBody>
-            <MultiSelectComponent
-              isLoading={loadingTemplates}
-              label={t("slide-form.slide-template-label")}
-              helpText={t("slide-form.slide-template-help-text")}
-              handleSelection={selectTemplate}
-              options={templateOptions}
-              selected={selectedTemplates}
-              name="templateInfo"
-              filterCallback={onFilterTemplate}
-              singleSelect
+            <FormInput
+              name="title"
+              type="text"
+              label={t("slide-form.slide-name-label")}
+              helpText={t("slide-form.slide-name-placeholder")}
+              value={slide.title || ""}
+              onChange={handleInput}
             />
           </ContentBody>
-        )}
-        {selectedTemplate && contentFormElements && (
-          <>
+          {templateOptions && (
             <ContentBody>
-              <h2 className="h4">{t("slide-form.preview-slide-title")}</h2>
-              <RemoteComponentWrapper
-                url={selectedTemplate?.resources?.component}
-                content={slide.content}
-                mediaFields={mediaFields}
-                mediaData={mediaData}
+              <MultiSelectComponent
+                label={t("slide-form.slide-template-label")}
+                helpText={t("slide-form.slide-template-help-text")}
+                handleSelection={selectTemplate}
+                options={templateOptions}
+                selected={selectedTemplates}
+                name="templateInfo"
+                filterCallback={onFilterTemplate}
+                singleSelect
               />
             </ContentBody>
-            <ContentBody>
-              {contentFormElements.map((formElement) => (
-                <RenderFormElement
-                  key={formElement.key}
-                  data={formElement}
-                  onChange={handleContent}
-                  onMediaChange={handleMedia}
-                  name={formElement.name}
+          )}
+          {selectedTemplate && contentFormElements && (
+            <>
+              <ContentBody>
+                <h2 className="h4">{t("slide-form.preview-slide-title")}</h2>
+                <RemoteComponentWrapper
+                  url={selectedTemplate?.resources?.component}
+                  slide={slide}
                   mediaData={mediaData}
-                  formStateObject={slide.content}
-                  requiredFieldCallback={() => {
-                    return false;
-                  }}
                 />
-              ))}
-            </ContentBody>
-          </>
-        )}
-        <ContentBody>
-          <h3 className="h4">{t("slide-form.slide-publish-title")}</h3>
-          <Row className="g-2">
-            <Col md>
-              <FormInput
-                name="published.from"
-                type="datetime-local"
-                label={t("slide-form.slide-from-label")}
-                value={slide.published.from ?? ""}
-                onChange={handleInput}
-              />
-            </Col>
-            <Col md>
-              <FormInput
-                name="published.to"
-                type="datetime-local"
-                label={t("slide-form.slide-to-label")}
-                value={slide.published.to ?? ""}
-                onChange={handleInput}
-              />
-            </Col>
-          </Row>
-        </ContentBody>
-        {themesOptions && (
+              </ContentBody>
+              <ContentBody>
+                {contentFormElements.map((formElement) => (
+                  <ContentForm
+                    key={formElement.key}
+                    data={formElement}
+                    onChange={handleContent}
+                    onMediaChange={handleMedia}
+                    name={formElement.name}
+                    mediaData={mediaData}
+                    formStateObject={slide.content}
+                    requiredFieldCallback={() => {
+                      return false;
+                    }}
+                  />
+                ))}
+              </ContentBody>
+            </>
+          )}
           <ContentBody>
-            <MultiSelectComponent
-              isLoading={loadingThemes}
-              label={t("slide-form.slide-theme-label")}
-              handleSelection={selectTheme}
-              options={themesOptions}
-              selected={selectedTheme}
-              name="theme"
-              filterCallback={onFilterTheme}
-              singleSelect
-            />
+            <h3 className="h4">{t("slide-form.slide-publish-title")}</h3>
+            <Row className="g-2">
+              <Col md>
+                <FormInput
+                  name="published.from"
+                  type="datetime-local"
+                  label={t("slide-form.slide-from-label")}
+                  value={slide.published.from ?? ""}
+                  onChange={handleInput}
+                />
+              </Col>
+
+              <Col md>
+                <FormInput
+                  name="published.to"
+                  type="datetime-local"
+                  label={t("slide-form.slide-to-label")}
+                  value={slide.published.to ?? ""}
+                  onChange={handleInput}
+                />
+              </Col>
+            </Row>
           </ContentBody>
-        )}
-        <ContentFooter>
-          <Button
-            variant="secondary"
-            type="button"
-            id="cancel_slide"
-            onClick={() => history.push("/slide/list/")}
-            size="lg"
-            className="me-3"
-          >
-            {t("slide-form.cancel-button")}
-          </Button>
-          <Button
-            variant="primary"
-            type="button"
-            onClick={handleSubmit}
-            id="save_slide"
-            size="lg"
-          >
-            {t("slide-form.save-button")}
-          </Button>
-        </ContentFooter>
-      </Form>
-    </>
+          {themesOptions && (
+            <ContentBody>
+              <MultiSelectComponent
+                label={t("slide-form.slide-theme-label")}
+                handleSelection={selectTheme}
+                options={themesOptions}
+                selected={selectedTheme}
+                name="theme"
+                filterCallback={onFilterTheme}
+                singleSelect
+              />
+            </ContentBody>
+          )}
+        </>
+      )}
+      <ContentFooter>
+        <Button
+          variant="secondary"
+          type="button"
+          id="cancel_slide"
+          onClick={() => history.push("/slide/list/")}
+          size="lg"
+          className="me-3"
+        >
+          {t("slide-form.cancel-button")}
+        </Button>
+        <Button
+          variant="primary"
+          type="button"
+          onClick={handleSubmit}
+          id="save_slide"
+          size="lg"
+        >
+          <>
+            {!isSaving && t("slide-form.save-button")}
+            {isSaving && (
+              <>
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                  className="m-1"
+                />
+                {t("slide-form.saving")}
+              </>
+            )}
+          </>
+        </Button>
+        <Toast show={isSaveSuccess} text={t("slide-form.saved")} />
+        <Toast show={!!errors} text={t("slide-form.error")} />
+      </ContentFooter>
+    </Form>
   );
 }
 
 SlideForm.defaultProps = {
   selectedTemplate: null,
-  loadingMessage: "",
-  isLoading: false,
   selectedTheme: [],
 };
 
@@ -261,7 +330,9 @@ SlideForm.propTypes = {
   slide: PropTypes.objectOf(PropTypes.any).isRequired,
   handleInput: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  isSaving: PropTypes.bool.isRequired,
   headerText: PropTypes.string.isRequired,
+  isSaveSuccess: PropTypes.bool.isRequired,
   selectTheme: PropTypes.func.isRequired,
   selectedTheme: PropTypes.arrayOf(
     PropTypes.shape({ "@id": PropTypes.string.isRequired })
@@ -274,8 +345,11 @@ SlideForm.propTypes = {
       component: PropTypes.string.isRequired,
     }).isRequired,
   }),
-  isLoading: PropTypes.bool,
-  loadingMessage: PropTypes.string,
+  isLoading: PropTypes.bool.isRequired,
+  errors: PropTypes.oneOfType([
+    PropTypes.objectOf(PropTypes.any),
+    PropTypes.bool,
+  ]).isRequired,
   handleContent: PropTypes.func.isRequired,
   handleMedia: PropTypes.func.isRequired,
   mediaData: PropTypes.objectOf(PropTypes.any).isRequired,
