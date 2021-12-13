7912a4fede3cd534d309923dc094356e34ef62ee
Sine <sinje@aarhus.dk>
2021-12-13 21:28:54 +0100

    AR-705: removed testdata

--- src/components/slide/slide-form.jsx
+++ src/components/slide/slide-form.jsx
@@ -84,29 +84,6 @@ function SlideForm({
         .then((response) => response.json())
         .then((data) => {
           setContentFormElements(data);
-          setContentFormElements([
-            {
-              key: "contacts-form-1",
-              input: "header",
-              text: "Skabelon: Kontakter",
-              name: "header1",
-              formGroupClasses: "h4 mb-3",
-            },
-
-            {
-              key: "contacts-form-2",
-              input: "checkbox",
-              label: "Animeret streg under overskrift",
-              name: "separator",
-              formGroupClasses: "mb-3",
-            },
-            {
-              key: "contacts-form-3",
-              input: "contacts",
-              name: "contacts",
-              formGroupClasses: "mb-3",
-            },
-          ]);
         })
         .catch(() => {
           // @TODO: Handle error case. Display error for user.
