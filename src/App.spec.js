6ebe8c1e94affc0450d857caad501ee91dc37210
Sine <sinje@aarhus.dk>
2021-12-20 08:56:35 +0100

    AR-716: added check

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,6 +4,7 @@ describe("Simple app loads", () => {
   it("Loads and simple test", () => {
     cy.visit("/slide/list?published=all&page=1&order=asc&sort=title");
     await(10000)
+    cy.get("tbody").find("tr").should("have.length", 10);
     // cy.visit("/slides");
     // cy.get("#root").should("not.be.empty");
   });
