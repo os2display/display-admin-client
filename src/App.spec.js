9871ec6d54377ed0bd6273ac52988986510e3ef1
Sine <sinje@aarhus.dk>
2021-12-20 09:00:00 +0100

    AR-716: change 10 to 11

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,7 +4,7 @@ describe("Simple app loads", () => {
   it("Loads and simple test", () => {
     cy.visit("/slide/list?published=all&page=1&order=asc&sort=title");
     cy.wait(10000)
-    cy.get("tbody").find("tr").should("have.length", 10);
+    cy.get("tbody").find("tr").should("have.length", 11);
     // cy.visit("/slides");
     // cy.get("#root").should("not.be.empty");
   });
