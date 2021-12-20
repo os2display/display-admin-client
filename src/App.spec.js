a170ff47771584c4f14b7b4813aa8515bc619732
Sine <sinje@aarhus.dk>
2021-12-20 08:53:13 +0100

    AR-716: test

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,10 @@
+import { wait } from '@testing-library/react';
+
 describe("Simple app loads", () => {
   it("Loads and simple test", () => {
-    cy.visit("/slides");
-    cy.get("#root").should("not.be.empty");
+    cy.visit("/slide/list?published=all&page=1&order=asc&sort=title");
+    await(10000)
+    // cy.visit("/slides");
+    // cy.get("#root").should("not.be.empty");
   });
 });
