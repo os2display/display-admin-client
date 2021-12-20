8082ca834b3d9831d01fd6661f0e15048d2639ea
Sine <sinje@aarhus.dk>
2021-12-20 08:58:02 +0100

    Ar-716: await now wait

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,7 +3,7 @@ import { wait } from '@testing-library/react';
 describe("Simple app loads", () => {
   it("Loads and simple test", () => {
     cy.visit("/slide/list?published=all&page=1&order=asc&sort=title");
-    await(10000)
+    cy.wait(10000)
     cy.get("tbody").find("tr").should("have.length", 10);
     // cy.visit("/slides");
     // cy.get("#root").should("not.be.empty");
