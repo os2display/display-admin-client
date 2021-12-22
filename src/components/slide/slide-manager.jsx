47574fe717349841334702f4c877b509e4338feb
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-12-22 22:54:34 +0100

    AR-685: Load feed data

--- src/components/slide/slide-manager.jsx
+++ src/components/slide/slide-manager.jsx
@@ -243,6 +243,14 @@ function SlideManager({
     if (initialState) {
       const localFormStateObject = JSON.parse(JSON.stringify(initialState));
 
+      if (initialState?.feed['@id']) {
+        dispatch(
+          api.endpoints.getV1FeedsByIdData.initiate({ id: idFromUrl(initialState.feed['@id']) })
+        ).then((response) => {
+          setFormStateObject({...localFormStateObject, feedData: response.data});
+        })
+      }
+
       // Make sure content is an object.
       if (localFormStateObject.content instanceof Array) {
         localFormStateObject.content = {};
