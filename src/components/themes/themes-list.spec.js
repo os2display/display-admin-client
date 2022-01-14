e0ad25aaea23281bc7946506a9f7647c9f845f02
Sine <sinje@aarhus.dk>
2022-01-14 11:40:21 +0100

    Ar-716: themes list

--- src/components/themes/themes-list.spec.js
+++ src/components/themes/themes-list.spec.js
@@ -11,28 +11,28 @@ describe("themes list tests", () => {
     cy.wait("@themesData");
   });
 
-  // it("It loads themes list", () => {
-  //   cy.visit("/themes/list");
-  //   cy.get("table").find("tbody").should("not.be.empty");
-  //   cy.get("tbody").find("tr td").should("exist");
-  // });
+  it("It loads themes list", () => {
+    cy.visit("/themes/list");
+    cy.get("table").find("tbody").should("not.be.empty");
+    cy.get("tbody").find("tr td").should("exist");
+  });
 
-  // it("It goes to edit (themes list)", () => {
-  //   cy.get("#themeTitle").should("not.exist");
-  //   cy.get("tbody").find("tr td a").eq(0).click();
-  //   cy.get("#themeTitle").should("exist");
-  // });
+  it("It goes to edit (themes list)", () => {
+    cy.get("#themeTitle").should("not.exist");
+    cy.get("tbody").find("tr td a").eq(0).click();
+    cy.get("#themeTitle").should("exist");
+  });
 
-  // it("It opens delete modal (themes list)", () => {
-  //   cy.get("#delete-modal").should("not.exist");
-  //   cy.get("tbody").find("tr td button").eq(2).should("be.disabled");
-  //   cy.get("tbody").find("tr").eq(1).find("td button").eq(1).click();
-  //   cy.get("#delete-modal").should("exist");
-  // });
+  it("It opens delete modal (themes list)", () => {
+    cy.get("#delete-modal").should("not.exist");
+    cy.get("tbody").find("tr td button").eq(2).should("be.disabled");
+    cy.get("tbody").find("tr").eq(1).find("td button").eq(1).click();
+    cy.get("#delete-modal").should("exist");
+  });
 
-  // it("The correct amount of column headers loaded (themes list)", () => {
-  //   cy.get("thead").find("th").should("have.length", 6);
-  // });
+  it("The correct amount of column headers loaded (themes list)", () => {
+    cy.get("thead").find("th").should("have.length", 6);
+  });
 
   it("It removes all selected", () => {
     cy.get("tbody")
