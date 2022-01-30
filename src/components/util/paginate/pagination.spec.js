87228623c88009318faf9886d9a9248a28656d07
Sine <sinje@aarhus.dk>
2022-01-30 20:49:28 +0100

    AR-716: updated pagination.soec

--- src/components/util/paginate/pagination.spec.js
+++ src/components/util/paginate/pagination.spec.js
@@ -1,18 +1,53 @@
 describe("Pagination loads", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/themes*", { fixture: "themes.json" }).as(
+      "themesData"
+    );
+    cy.visit("/themes/list?page=1&order=asc&sort=title");
+    cy.wait(["@themesData"]);
+  });
+
   it("It loads", () => {
-    cy.visit("/tags");
     cy.get(".pagination").should("not.be.empty");
   });
 
-  it("It works", () => {
-    cy.visit("/tags");
+  it("It works on themes", () => {
     cy.get(".pagination")
       .find("button")
       .first()
       .invoke("text")
       .should("match", /^1/);
-    cy.get("tbody").find("tr").should("have.length", 10);
-    cy.get(".pagination").find("button").last().click();
-    cy.get("tbody").find("tr").should("have.length", 3);
+    cy.get("tbody")
+      .find("tr td")
+      .eq(1)
+      .invoke("text")
+      .then((titleCellText) => {
+        cy.get("tbody").find("tr").should("have.length", 10);
+
+        cy.intercept("GET", "**/themes*", {
+          fixture: "themes-second-page.json",
+        }).as("themesData");
+
+        cy.get(".pagination").find(".page-link").eq(9).click();
+        cy.wait(["@themesData"]);
+        cy.get("tbody").find("tr").should("have.length", 10);
+        cy.get("tbody")
+          .find("tr td")
+          .eq(1)
+          .should("not.have.text", titleCellText);
+      });
+  });
+
+  it("It works on themes with url input", () => {
+    cy.intercept("GET", "**/themes*", { fixture: "themes.json" }).as(
+      "themesData"
+    );
+    cy.visit("/themes/list?page=6&order=asc&sort=title");
+    cy.wait(["@themesData"]);
+
+    cy.get(".pagination")
+      .find(".page-item")
+      .eq(5)
+      .should("have.class", "active");
   });
 });
