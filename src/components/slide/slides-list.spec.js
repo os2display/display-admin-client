2acb4860ec6c1f97a28d17db35236375b0f2e58a
Sine <sinje@aarhus.dk>
2022-01-30 20:49:03 +0100

    AR-716: updated slides-list.spec

--- src/components/slide/slides-list.spec.js
+++ src/components/slide/slides-list.spec.js
@@ -1,37 +1,38 @@
-Cypress.on("uncaught:exception", () => {
-  // @TODO: fix when docker setup is fixed
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
-});
-
 describe("Slides list tests", () => {
   beforeEach(() => {
-    cy.visit("/slides");
+    cy.intercept({
+      method: "GET",
+      url: "**/templates/*",
+    }).as("templatesData");
+    cy.visit("/slide/list?published=all&page=2&order=asc&sort=title");
+    cy.wait([
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+      "@templatesData",
+    ]);
   });
+
   it("It loads slides list", () => {
     cy.get("table").find("tbody").should("not.be.empty");
-    cy.get("tbody").find("tr td").should("have.length", 100);
+    cy.get("tbody").find("tr td").should("exist");
   });
 
   it("It goes to edit (slides list)", () => {
-    cy.get("#slideName").should("not.exist");
-    cy.get("tbody").find("tr td a").eq(0).click();
-    cy.get("#slideName").should("exist");
-  });
-  it("It opens delete modal (slides list)", () => {
-    cy.get("#delete-modal").should("not.exist");
-    cy.get("tbody").find("tr td button").eq(4).click();
-    cy.get("#delete-modal").should("exist");
+    cy.get("#slidesTitle").should("not.exist");
+    cy.get("tbody").find(".edit-button").eq(0).click();
+    cy.get("#slidesTitle").should("exist");
   });
 
   it("The correct amount of column headers loaded (slides list)", () => {
-    cy.visit("/slides");
-    cy.get("thead").find("th").should("have.length", 10);
+    cy.get("thead").find("th").should("have.length", 7);
   });
 
   it("It removes all selected", () => {
-    cy.visit("/slides");
     cy.get("tbody").find("tr td button").eq(0).click();
     cy.get("tbody").find("tr").eq(0).should("have.class", "bg-light");
     cy.get("#clear-rows-button").click();
