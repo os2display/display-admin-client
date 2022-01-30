5634282a0e52c2531b6ad9c750b56aa2e580b23f
Sine <sinje@aarhus.dk>
2022-01-30 20:49:11 +0100

    AR-716: created theme.spec

---
+++ src/components/themes/theme.spec.js
@@ -0,0 +1,57 @@
+describe("Theme pages work", () => {
+  it("It loads create theme page", () => {
+    cy.visit("/themes/create");
+    cy.get("#save_theme").should("exist");
+  });
+
+  it("It redirects on save", () => {
+    cy.visit("/themes/create");
+
+    // Mock error response on post
+    cy.intercept("POST", "**/themes", {
+      statusCode: 201,
+      fixture: "save-themes-response.json",
+    });
+
+    // Mock successful response on get
+    cy.intercept("GET", "**/themes/*", {
+      fixture: "save-themes-response.json",
+    });
+
+    // Displays success toast and redirects
+    cy.get(".Toastify").find(".Toastify__toast--success").should("not.exist");
+    cy.get("#save_theme").click();
+    cy.get(".Toastify").find(".Toastify__toast--success").contains("gemt");
+    cy.url().should("include", "themes/edit");
+
+    cy.get("#title")
+      .invoke("val")
+      .should("match", /^title/);
+  });
+
+  it("It display error toast on save error", () => {
+    cy.visit("/themes/create");
+
+    // Mock error response on post
+    cy.intercept("POST", "**/themes", {
+      statusCode: 500,
+      fixture: "error.json",
+    });
+
+    // Displays error toast and stays on page
+    cy.get(".Toastify").find(".Toastify__toast--error").should("not.exist");
+    cy.get("#save_theme").click();
+    cy.get(".Toastify").find(".Toastify__toast--error").should("exist");
+    cy.get(".Toastify")
+      .find(".Toastify__toast--error")
+      .contains("Errorerrorerror");
+    cy.url().should("include", "themes/create");
+  });
+
+  it("It cancels create theme", () => {
+    cy.visit("/themes/create");
+    cy.get("#cancel_theme").should("exist");
+    cy.get("#cancel_theme").click();
+    cy.get("#cancel_theme").should("not.exist");
+  });
+});
