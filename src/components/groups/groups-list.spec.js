7bbbc4c241b41db309d5400eea5c81797ef682f6
Sine <sinje@aarhus.dk>
2022-01-30 20:47:33 +0100

    AR-716: updated groups-list.spec

--- src/components/groups/groups-list.spec.js
+++ src/components/groups/groups-list.spec.js
@@ -1,30 +1,37 @@
 describe("Groups list tests", () => {
+  beforeEach(() => {
+    cy.intercept({
+      method: "GET",
+      url: "**/screen-groups*",
+      query: {
+        page: "1",
+      },
+    }).as("screenGroupsData");
+    cy.visit("/group/list");
+    cy.wait(["@screenGroupsData"]);
+    cy.wait(1000);
+  });
   it("It loads groups list", () => {
-    cy.visit("/groups");
     cy.get("table").find("tbody").should("not.be.empty");
-    cy.get("tbody").find("tr td").should("have.length", 20);
+    cy.get("tbody").find("tr td").should("exist");
   });
 
   it("It goes to edit (groups list)", () => {
-    cy.visit("/groups");
-    cy.get("#groupName").should("not.exist");
+    cy.get("#groupTitle").should("not.exist");
     cy.get("tbody").find("tr td a").eq(0).click();
-    cy.get("#groupName").should("exist");
+    cy.get("#groupTitle").should("exist");
   });
   it("It opens delete modal (groups list)", () => {
-    cy.visit("/groups");
     cy.get("#delete-modal").should("not.exist");
     cy.get("tbody").find("tr td button").eq(1).click();
     cy.get("#delete-modal").should("exist");
   });
 
   it("The correct amount of column headers loaded (groups list)", () => {
-    cy.visit("/groups");
     cy.get("thead").find("th").should("have.length", 5);
   });
 
   it("It removes all selected", () => {
-    cy.visit("/groups");
     cy.get("tbody").find("tr td button").eq(0).click();
     cy.get("tbody").find("tr").eq(0).should("have.class", "bg-light");
     cy.get("#clear-rows-button").click();
