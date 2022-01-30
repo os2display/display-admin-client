e9aa77793429cb214e24bdcafc6d40b4fc5d5e4f
Sine <sinje@aarhus.dk>
2022-01-30 20:47:17 +0100

    AR-716: updated group.spec

--- src/components/groups/group.spec.js
+++ src/components/groups/group.spec.js
@@ -1,29 +1,32 @@
-Cypress.on("uncaught:exception", () => {
-  // @TODO: fix when docker setup is fixed
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
-});
-
-describe("group pages work", () => {
+describe("Group pages work", () => {
   it("It loads create group page", () => {
     cy.visit("/group/create");
-    cy.get("#save_group")
-      .invoke("text")
-      .should("match", /^Gem group/);
-  });
-
-  it("It loads create group page", () => {
-    cy.visit("/group/create");
-    cy.get("#save_group")
-      .invoke("text")
-      .should("match", /^Gem gruppe/);
+    cy.get("#save_group").should("exist");
   });
 
   it("It redirects on save", () => {
     cy.visit("/group/create");
+
+    // Mock error response on post
+    cy.intercept("POST", "**/screen-groups", {
+      statusCode: 201,
+      fixture: "save-groups-response.json",
+    });
+
+    // Mock successful response on get
+    cy.intercept("GET", "**/screen-groups/*", {
+      fixture: "save-groups-response.json",
+    });
+
+    // Displays success toast and redirects
+    cy.get(".Toastify").find(".Toastify__toast--success").should("not.exist");
     cy.get("#save_group").click();
+    cy.get(".Toastify").find(".Toastify__toast--success").contains("gemt");
     cy.url().should("include", "group/edit/");
+
+    cy.get("#title")
+      .invoke("val")
+      .should("match", /^title/);
   });
 
   it("It cancels create group", () => {
@@ -31,5 +34,23 @@ describe("group pages work", () => {
     cy.get("#cancel_group").should("exist");
     cy.get("#cancel_group").click();
     cy.get("#cancel_group").should("not.exist");
+  });
+
+  it("It display error toast on save error", () => {
+    cy.visit("/group/create");
+
+    // Mock error response on post
+    cy.intercept("POST", "**/screen-groups", {
+      statusCode: 500,
+      fixture: "error.json",
+    });
+
+    // Displays error toast and stays on page
+    cy.get(".Toastify").find(".Toastify__toast--error").should("not.exist");
+    cy.get("#save_group").click();
+    cy.get(".Toastify")
+      .find(".Toastify__toast--error")
+      .contains("Errorerrorerror");
+    cy.url().should("include", "group/create");
   });
 });
