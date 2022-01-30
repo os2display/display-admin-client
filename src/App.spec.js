c5f826e6b5a730be35d71640f9ca8479f010bb8f
Sine <sinje@aarhus.dk>
2022-01-30 20:47:00 +0100

    Ar-716: updated app.spec

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,7 @@
 describe("Simple app loads", () => {
   it("Loads and simple test", () => {
-    cy.visit("/slides");
+    cy.visit("/slide/list?published=all&page=1&order=asc&sort=title");
+    cy.get("tbody").find("tr").should("have.length", 10);
     cy.get("#root").should("not.be.empty");
   });
 });
