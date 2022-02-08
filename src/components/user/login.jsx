d89868c7db14d3ae015314e76b4697cbb7cedb24
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-02-08 11:04:18 +0100

    AR-793: Make sure component is mounted for fetch promises

--- src/components/user/login.jsx
+++ src/components/user/login.jsx
@@ -15,11 +15,12 @@ import { api } from "../../redux/api/api.generated";
  */
 function Login() {
   const { t } = useTranslation("common");
+  const { search } = useLocation();
   const dispatch = useDispatch();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const { search } = useLocation();
   const [oidcAuthUrls, setOidcAuthUrls] = useState("");
   const [oidcAuthLoadingError, setOidcAuthLoadingError] = useState("");
   const [ready, setReady] = useState(false);
@@ -66,6 +67,7 @@ function Login() {
   };
 
   useEffect(() => {
+    let isMounted = true;
     let idToken = null;
     let state = null;
 
@@ -85,18 +87,24 @@ function Login() {
       )
         .then((resp) => resp.json())
         .then((data) => {
-          if (data?.token) {
-            localStorage.setItem("api-token", data.token);
+          if (isMounted) {
+            if (data?.token) {
+              localStorage.setItem("api-token", data.token);
 
-            const event = new Event("authenticated");
-            document.dispatchEvent(event);
+              const event = new Event("authenticated");
+              document.dispatchEvent(event);
+            }
           }
         })
         .catch(() => {
-          setOidcAuthLoadingError(t("login.error-oidc-login"));
+          if (isMounted) {
+            setOidcAuthLoadingError(t("login.error-oidc-login"));
+          }
         })
         .finally(() => {
-          setReady(true);
+          if (isMounted) {
+            setReady(true);
+          }
         });
     } else {
       fetch(
@@ -108,45 +116,51 @@ function Login() {
       )
         .then((resp) => {
           resp.json().then((data) => {
-            setOidcAuthUrls(data);
+            if (isMounted) {
+              setOidcAuthUrls(data);
+            }
           });
         })
         .catch(() => {
-          setOidcAuthLoadingError(t("login.error-fetching-oidc-urls"));
+          if (isMounted) {
+            setOidcAuthLoadingError(t("login.error-fetching-oidc-urls"));
+          }
         })
         .finally(() => {
-          setReady(true);
+          if (isMounted) {
+            setReady(true);
+          }
         });
     }
+
+    return () => {
+      isMounted = false;
+    };
   }, [search]);
 
   return (
     <>
       {ready && (
         <Card className="m-5 bg-light">
-          <h3 className="m-3">{t("login.please-authenticate")}</h3>
-
           <Form onSubmit={onSubmit} className="m-3">
-            <Row className="g-2">
+            <Row>
               <Col md>
-                <div className="mb-4">
-                  <h5 className="mb-3">{t("login.login-with-oidc")}</h5>
-                  {oidcAuthUrls && (
-                    <Button
-                      className="btn btn-primary"
-                      type="button"
-                      href={oidcAuthUrls.authorizationUrl}
-                    >
-                      {t("login.login-with-oidc")}
-                    </Button>
-                  )}
-                  {oidcAuthLoadingError && (
-                    <Alert variant="danger">{oidcAuthLoadingError}</Alert>
-                  )}
-                </div>
+                <h3 className="mb-3">{t("login.login-with-oidc")}</h3>
+                {oidcAuthUrls && (
+                  <Button
+                    className="btn btn-primary"
+                    type="button"
+                    href={oidcAuthUrls.authorizationUrl}
+                  >
+                    {t("login.login-with-oidc")}
+                  </Button>
+                )}
+                {oidcAuthLoadingError && (
+                  <Alert variant="danger">{oidcAuthLoadingError}</Alert>
+                )}
               </Col>
               <Col md>
-                <h5>{t("login.login-with-username-password")}</h5>
+                <h3>{t("login.login-with-username-password")}</h3>
                 <FormInput
                   onChange={onChange}
                   value={email}
