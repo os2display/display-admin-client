b10059ff69c5847ad30139425022fc613ab53a9c
Sine <sinje@aarhus.dk>
2022-03-23 14:17:58 +0100

    add url

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,8 @@ module.exports = function override(config, env) {
   };
   config.resolve.fallback = {
     "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify")
+    "https": require.resolve("https-browserify"),
+    "url": require.resolve("url/")
   };
 
   return config;
