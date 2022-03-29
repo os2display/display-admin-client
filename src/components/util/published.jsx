0e1587dfd8ec6662acaa701b23042748461def74
Sine <sinje@aarhus.dk>
2022-03-29 09:08:56 +0200

    AR-611: published dates in lists

--- src/components/util/published.jsx
+++ src/components/util/published.jsx
@@ -1,5 +1,8 @@
 import { React, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import dayjs from "dayjs";
+import localeDa from "dayjs/locale/da";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 import { useTranslation } from "react-i18next";
 import calculateIsPublished from "./helpers/calculate-is-published";
 
@@ -9,14 +12,40 @@ import calculateIsPublished from "./helpers/calculate-is-published";
  * @returns {object} The published yes/no component.
  */
 function Published({ published }) {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation("common", { keyPrefix: "published" });
   const [isPublished, setIsPublished] = useState(false);
+
+  useEffect(() => {
+    dayjs.extend(localizedFormat);
+  }, []);
 
   useEffect(() => {
     setIsPublished(calculateIsPublished(published));
   }, [published]);
 
-  return <div>{isPublished ? t("published.yes") : t("published.no")}</div>;
+  if (!published.from && !published.to) {
+    return <div>{isPublished ? t("yes") : t("no")}</div>;
+  }
+
+  let publishedFrom = "-";
+  let publishedTo = "-";
+
+  if (published.from) {
+    publishedFrom = dayjs(published.from).locale(localeDa).format("LLLL");
+  }
+  if (published.to) {
+    publishedTo = dayjs(published.to).locale(localeDa).format("LLLL");
+  }
+  return (
+    <>
+      <div>
+        {t("from")}: {publishedFrom}
+      </div>
+      <div>
+        {t("to")}: {publishedTo}
+      </div>
+    </>
+  );
 }
 
 Published.propTypes = {
