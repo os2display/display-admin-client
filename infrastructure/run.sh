83ae12c0baaaad0b9fdd53ba22cb4fab80064e72
Jesper Kristensen <cableman@linuxdev.dk>
2022-11-16 12:47:55 +0100

    AR-875: Updated docker setup to match new itkdev base setup

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -eux
+
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-admin-client:${VERSION} --file="Dockerfile" .
+docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-admin-client:${VERSION} --file="Dockerfile" .
 
-docker push itkdev/os2display-admin-client:${VERSION}
+# docker push itkdev/os2display-admin-client:${VERSION}
