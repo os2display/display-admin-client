6a7fe56f2b212d293c1c328691d93d658c1de706
Ture Gjørup <ture@ignatz.dk>
2023-06-14 08:58:48 +0200

    DISPLAY-986: Update docker build to publish to "os2display" org on docker hup. Update github workflow to latest actions.

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -5,6 +5,6 @@ set -eux
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-admin-client:${VERSION} --file="Dockerfile" .
+docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=os2display/os2display-admin-client:${VERSION} --file="Dockerfile" .
 
-# docker push itkdev/os2display-admin-client:${VERSION}
+# docker push os2display/os2display-admin-client:${VERSION}
