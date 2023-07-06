55d4da38bcc71f5e14c774f1b5fd814b7b8401dd
Ture Gjørup <ture@ignatz.dk>
2023-07-06 10:30:22 +0200

    SUPP0RT-1109: Rename image form os2display/os2display-admin-client -> os2display/admin-client

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -5,6 +5,6 @@ set -eux
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=os2display/os2display-admin-client:${VERSION} --file="Dockerfile" .
+docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=os2display/admin-client:${VERSION} --file="Dockerfile" .
 
-# docker push os2display/os2display-admin-client:${VERSION}
+# docker push os2display/admin-client:${VERSION}
