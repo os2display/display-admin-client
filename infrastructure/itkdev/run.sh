513004601e6643ec34ef7518a39c4e6e2f92dfcb
Ture Gjørup <ture@ignatz.dk>
2023-07-11 09:59:44 +0200

    DISPLAY-986: Fix wrong image name

--- infrastructure/itkdev/run.sh
+++ infrastructure/itkdev/run.sh
@@ -5,6 +5,6 @@ set -eux
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=os2display/display-admin-client:${VERSION} --file="Dockerfile" .
+docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-admin-client:${VERSION} --file="Dockerfile" .
 
-# docker push os2display/display-admin-client:${VERSION}
+# docker push itkdev/os2display-admin-client:${VERSION}
