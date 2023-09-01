49313096085ebcd5cf1738e86b38f288d0a836b5
Sine Jespersen <sinejespersen@gmail.com>
2023-09-01 11:50:08 +0200

    DISPLAY-1009: apply coding standards

--- src/components/users/user-columns.jsx
+++ src/components/users/user-columns.jsx
@@ -1,4 +1,3 @@
-import { React } from "react";
 import { useTranslation } from "react-i18next";
 import SelectColumnHoc from "../util/select-column-hoc";
 import ColumnHoc from "../util/column-hoc";
@@ -6,12 +5,6 @@ import ColumnHoc from "../util/column-hoc";
 /**
  * Columns for Users lists.
  *
- * @param {object} props - The props.
- * @param {Function} props.apiCall - The api to call
- * @param {string} props.infoModalRedirect - The url for redirecting in the info modal.
- * @param {string} props.infoModalTitle - The info modal title.
- * @param {string} props.dataKey The data key for mapping the data.
- * @param {Function} props.handleDelete
  * @returns {object} The columns for the users lists.
  */
 function getUserColumns() {
