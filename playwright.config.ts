ff08f4f881d280492daf330e2a8c4f32853e10c4
Sine Jespersen <sinejespersen@gmail.com>
2024-06-03 14:04:01 +0200

    1541: add reporter: [['html', { open: 'never' }]] to playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Open never added to avoid the CI pipeline to get stuck in a html-reporter-mode.
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
